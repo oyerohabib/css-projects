70e06f1108d25f8d17c4d99eafbb9f5c1ac97d58
oyerohabib <oyerohabib@yahoo.com>
2021-08-02 07:59:58 +0900

    completed restaurants attendance

---
+++ restaurants_website/assets/js/script.js
@@ -0,0 +1,49 @@
+window.addEventListener("load", function(){
+  // page loader
+  document.querySelector(".page-loader").classList.add("fade-out");
+  setTimeout(function() {
+    document.querySelector(".page-loader").getElementsByClassName.display = "none"
+  })
+  // AOS
+  AOS.init();
+})
+
+// toggle navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav() {
+  navToggler.classList.toggle("active")
+  document.querySelector(".nav").classList.toggle("open");
+}
+
+// close nav when click on nav element
+document.addEventListener("click", function(e){
+  if(e.target.closest(".nav-item")){
+    toggleNav()
+  }
+})
+
+// sticky header
+window.addEventListener("scroll", function(){
+  if(this.pageYOffset > 60) {
+    document.querySelector(".header").classList.add("sticky");
+  } else {
+    document.querySelector(".header").classList.remove("sticky");
+  }
+})
+
+// menu tabs start
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e){
+  if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")) {
+    const target = e.target.getAttribute("data-target");
+    menuTabs.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    const menuSection = document.querySelector(".menu-section");
+    menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+    menuSection.querySelector(target).classList.add("active");
+    // AOS
+    AOS.init();
+  }
+})
